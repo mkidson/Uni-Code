788564e53a5a0ccf2936090ea436e1b6c6241d64
MilesKidson <miles.kidson@gmail.com>
2020-03-09 14:05:57 +0200

    idk dude

--- 2NA Assignments/Assignment 1/Q2.py
+++ 2NA Assignments/Assignment 1/Q2.py
@@ -16,7 +16,7 @@ y = np.linspace(-10, 10, 500)
 def y1(x):
     return (x**2)-2
 def y2(x):
-    return np.sqrt(np.abs(x)+2)
+    return np.sqrt(x+2)
 def y3(x):
     return 2/(x-1)
 def y4(x):
@@ -24,16 +24,18 @@ def y4(x):
 
 y32 = y3(x)[~np.isnan(y3(x))]
 
-plt.plot(x, y1(x), color='r', lw=0.5, label='$y = x^2-2$')
-plt.plot(np.abs(x), y2(x), color='b', lw=0.5, label='$y = \sqrt{x+2}$')
-plt.plot(x, y32, color='y', lw=0.5, label='$y = \\frac{2}{x-1}$')
-plt.plot(x, y4(x), color='g', lw=0.5, label='$y = \\frac{2}{x}+1$')
+plt.plot(x, y1(x), color='r', lw=0.5, label='$y = g_1(x)')
+plt.plot(x, y2(x), color='b', lw=0.5, label='$y = g_2(x)$')
+plt.plot(x, y32, color='y', lw=0.5, label='$y = g_3(x)$')
+plt.plot(x, y4(x), color='g', lw=0.5, label='$y = g_4(x)$')
 plt.plot(x, np.zeros(500), color='black')
 plt.plot(np.zeros(500), y, color='black')
 plt.plot(x, y, label='$y = x$')
 
 plt.ylim(-10, 10)
 plt.xlim(-10, 10)
+plt.xlabel('x')
+plt.ylabel('y', rotation=0)
 plt.legend()
 plt.savefig('2NA Assignments\Assignment 1\Q2_Plot.pgf')
 # plt.show()
