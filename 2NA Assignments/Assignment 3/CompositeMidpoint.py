a0c86c87f521fff3048dceb4afd487455a29b5fc
MilesKidson <miles.kidson@gmail.com>
2020-06-05 18:08:33 +0200

    Ass 3 NA ok now it's properly done

--- 2NA Assignments/Assignment 3/CompositeMidpoint.py
+++ 2NA Assignments/Assignment 3/CompositeMidpoint.py
@@ -4,12 +4,11 @@ a = 0
 b = 1
 n = 1000
 h = (b-a)/n
-xs = np.linspace(a, b, n)
 I = 0
 # Looping through the summation, setting a new w at each iteration
-for i in xs:
-    w = a+(i+(1/2))*h
-    I += 4/(1+(i**2))
+for i in range(1000):
+    w = a+((i+(1/2))*h)
+    I += 4/(1+(w**2))
 # Multiplying by h as per the formula
 I *= h
 print(I)
