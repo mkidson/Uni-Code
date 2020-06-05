1780faae0d3d180cc5474958525d3419b340302a
MilesKidson <miles.kidson@gmail.com>
2020-06-05 12:39:41 +0200

    Starting NA Assignment 3\

---
+++ 2NA Assignments/Assignment 3/CompositeMidpoint.py
@@ -0,0 +1,13 @@
+from matplotlib import pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
+from numpy import cos, pi, sin, sqrt, exp, random
+import matplotlib
+#matplotlib.use('pgf')
+matplotlib.rcParams.update({
+    'pgf.texsystem': 'pdflatex',
+    'font.family': 'serif',
+    'text.usetex': True,
+    'pgf.rcfonts': False,
+})
+
