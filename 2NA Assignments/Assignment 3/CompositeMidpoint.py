cf43776fd399124bed12208b94e4b6e43f85c94c
MilesKidson <miles.kidson@gmail.com>
2020-06-05 16:41:58 +0200

    Halfway through Ass 3 NA

--- 2NA Assignments/Assignment 3/CompositeMidpoint.py
+++ 2NA Assignments/Assignment 3/CompositeMidpoint.py
@@ -1,13 +1,15 @@
-from matplotlib import pyplot as plt
 import numpy as np
-from scipy.optimize import curve_fit
-from numpy import cos, pi, sin, sqrt, exp, random
-import matplotlib
-#matplotlib.use('pgf')
-matplotlib.rcParams.update({
-    'pgf.texsystem': 'pdflatex',
-    'font.family': 'serif',
-    'text.usetex': True,
-    'pgf.rcfonts': False,
-})
-
+# Setting constants to allow for modification if need be
+a = 0
+b = 1
+n = 1000
+h = (b-a)/n
+xs = np.linspace(a, b, n)
+I = 0
+# Looping through the summation, setting a new w at each iteration
+for i in xs:
+    w = a+(i+(1/2))*h
+    I += 4/(1+(i**2))
+# Multiplying by h as per the formula
+I *= h
+print(I)
