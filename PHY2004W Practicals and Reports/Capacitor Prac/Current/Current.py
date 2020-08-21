351120b626f651b2b3d518f6fcf8375bee6e39d7
MilesKidson <miles.kidson@gmail.com>
2020-08-21 02:26:47 +0200

    fuck

--- PHY2004W Practicals and Reports/Capacitor Prac/Current/Current.py
+++ PHY2004W Practicals and Reports/Capacitor Prac/Current/Current.py
@@ -3,7 +3,7 @@ import numpy as np
 from numpy import cos, pi, sin, sqrt, exp, random
 from scipy.signal import find_peaks
 import matplotlib
-matplotlib.use('pgf')
+# matplotlib.use('pgf')
 matplotlib.rcParams.update({
     'pgf.texsystem': 'pdflatex',
     'font.family': 'serif',
@@ -31,19 +31,24 @@ for line in lines:
     data[3][i] = float(columns[3])
     i += 1
 file.close()
-# data[1]=data[1]/15000
 # Finding the peaks of each
 ResPeaks=find_peaks(data[1], distance=3)
 AppPeaks=find_peaks(data[3], distance=3)
+# Calculating the average wavelength from both sets
 pi2Arr=np.concatenate((data[0][ResPeaks[0][1]]-data[0][ResPeaks[0][0]], data[0][ResPeaks[0][2]]-data[0][ResPeaks[0][1]], data[0][ResPeaks[0][3]]-data[0][ResPeaks[0][2]], data[0][AppPeaks[0][1]]-data[0][AppPeaks[0][0]], data[0][AppPeaks[0][2]]-data[0][AppPeaks[0][1]], data[0][AppPeaks[0][3]]-data[0][AppPeaks[0][2]]),axis=None)
-pi2=np.average(pi2Arr)
-print(pi2)
+pi2=np.mean(pi2Arr,dtype=np.float32)
+# Calculating the average difference in seconds between relevant peaks from each set
 diffs=np.concatenate((data[0][AppPeaks[0][0]]-data[0][ResPeaks[0][0]], data[0][AppPeaks[0][1]]-data[0][ResPeaks[0][1]], data[0][AppPeaks[0][2]]-data[0][ResPeaks[0][2]], data[0][AppPeaks[0][3]]-data[0][ResPeaks[0][3]]), axis=None)
-diff=np.average(diffs)
-print(diff)
-print(diff/pi2)
-print(2*diff/pi2)
-
+diff=np.mean(diffs,dtype=np.float32)
+radiansDiff=2*diff/pi2
+# Uncertainty calculations
+pi2Un=np.std(pi2Arr,dtype=np.float32)/sqrt(np.size(pi2Arr))
+diffUn=np.std(diffs,dtype=np.float16)/sqrt(np.size(diffs))
+radiansDiffUn=radiansDiff*sqrt((diffUn/diff)**2 + (pi2Un/pi2)**2)
+# Printing values and uncertainties
+print('wavelength:', pi2, '+/-', pi2Un)
+print('avg time difference:', diff, '+/-', diffUn)
+print('phase diff in pi radians:', radiansDiff, '+/-', radiansDiffUn)
 # Plotting both the applied voltage and the voltage measured across the resistor against time
 plt.figure()
 plt.plot(data[2], data[3], label='Applied Voltage', color='red', lw=0.5)
@@ -53,4 +58,4 @@ plt.legend(loc=1)
 plt.xlabel("Time (s)")
 plt.ylabel("Voltage (V)")
 # plt.show()
-plt.savefig('PHY2004W Practicals and Reports\Capacitor Prac\Current\Res_100Hz_15k.pgf')
+# plt.savefig('PHY2004W Practicals and Reports\Capacitor Prac\Current\Res_100Hz_15k.pgf')
