798dc70989b1565a6601e1bd87fb43fb7351abd2
MilesKidson <miles.kidson@gmail.com>
2021-04-28 23:13:08 +0200

    neither shooting or finite differences are working for NK. don't know why. upsetting

--- 3rd Year/3AN/Project 1/NK_FiniteDifference.py
+++ 3rd Year/3AN/Project 1/NK_FiniteDifference.py
@@ -17,17 +17,13 @@ alpha=0
 beta=1
 p=(beta-alpha)/(b-a)
 
-N=50
+N=100
 h=(b-a)/N
 print('h',h)
 
-r_span=np.array([a,b])
 rs=np.linspace(a,b,N)
 u_0=p*rs # Initial guess
 u_1=p # Initial guess first derivative
-particularICs=[0,0]
-homogICs=[0,1]
-
 
 def f(r):
     return -((1/r)*u_1 + ((p*r)/(1-(p*r)**2))*(u_1**2 - (n**2)/(r**2)) + (p*r)*(1-(p*r)**2))
@@ -41,31 +37,22 @@ def P(r):
 def N(r):
     return f(r)
 
-def particular(r,u):
-    y=u[0]
-    y1=u[1]
-    return np.array([y1, P(r)*y1 + Q(r)*y + N(r)])
+# Trying to implement finite difference method instead of linear shooting
+ps=P(rs[1:-1])
+qs=Q(rs[1:-1])
+ns=N(rs[1:-1])
 
-def homog(r,u):
-    w=u[0]
-    w1=u[1]
-    return np.array([w1, P(r)*w1 + Q(r)*w])
+A=np.diag((-2-(h**2)*qs), k=0)+np.diag((1+(h*ps[1:])/2),k=-1)+np.diag((1-(h*ps[:-1])/2),k=1)
+B=(h**2)*ns
+B[-1]+=(1-(h*ps[-1])/2)
 
+c1=np.matmul(np.linalg.inv(A),B)
+c=np.insert(c1,0,[0])
+c=np.append(c,[0])
+final=u_0+c
 
-ySoln = solve_ivp(particular, r_span, particularICs, t_eval=rs, method='RK45')
-wSoln = solve_ivp(homog, r_span, homogICs, t_eval=rs, method='RK45')
-t=ySoln.t
-
-C=(-ySoln.y[0][-1])/wSoln.y[0][-1]
-# print(C)
-z=ySoln.y[0]+C*wSoln.y[0]
-final=u_0+z
-
-plt.plot(t, z, label='z')
-# plt.plot(t, ySoln.y[0], label='y')
-# plt.plot(t, wSoln.y[0], label='w')
-plt.plot(t, final, label='u')
-plt.plot(t, u_0, label='u0')
-# plt.plot(rs[2:-1], Q(rs[2:-1]), label='Q')
+plt.plot(rs, final, label='u(finite difference)')
+plt.plot(rs, u_0, label='u0')
+plt.plot(rs, c, label='c')
 plt.legend()
 plt.show()
--- 3rd Year/3AN/Project 1/NonlinearShooting.py
+++ 3rd Year/3AN/Project 1/NonlinearShooting.py
@@ -14,13 +14,13 @@ matplotlib.rcParams.update({
 In order to make this work for vorticity of 3, N needs to be greater than 50 and maxIter needs to be greater than 200. Not sure why this is but it seems to be fine. 
 """
 
-n=3 # Vorticity
-a=0.001 # Start of r
+n=1 # Vorticity
+a=0.001 # Start of r, should be 0 but that leads to problems
 b=10 # End of r, meant to be infinity
 
 N=100 # Number of integration points
 h=(b-a)/N
-print('h',h)
+# print('h',h)
 
 r_span=np.array([a,b]) # Array containing start and end of r, used for solve_ivp
 rs=np.linspace(a,b,N) # Array of rs, needs to be defined for solve_ivp or else it will make its own array, which could be bad
@@ -28,7 +28,7 @@ rs=np.linspace(a,b,N) # Array of rs, needs to be defined for solve_ivp or else i
 alpha=0 # Left BC
 beta=1 # Right BC
 p=(beta-alpha)/(b-a) # Initial guess for the shooting parameter (gradient at r=0)
-print('p', p)
+print('Initial p:', p)
 ICs=np.array([alpha,p,0,1]) # Initial conditions [u(0)=alpha, v(0)=p, z(0)=0, w(0)=1]
 
 # These are just for convenience, could just be done explicitly in the du_dr function
@@ -41,18 +41,20 @@ def df_du(r,u,v):
 def df_dv(r,u,v):
     return -((1/r) + u/(1-u**2)*(2*v))
 
+# The function solve_ivp will use to solve the 2 IVPs
+def du_dr(R,y):
+    U=y[0]
+    V=y[1]
+    Z=y[2]
+    W=y[3]
+    return np.array([V, f(R,U,V), W, df_du(R,U,V)*Z + df_dv(R,U,V)*W])
 u_1=0 # Just used so the while doesn't break the first loop around
 tol=1e-5 # The program never seems to reach this level of accuracy but i'm leaving it in for the sake of completeness
 maxIter=400 # If this is too low then the higher vorticities don't reach a stable solution
 k=1 # Counter variable
-while k <= maxIter and (abs(u_1-b)>tol):
-    def du_dr(R,y):
-        U=y[0]
-        V=y[1]
-        Z=y[2]
-        W=y[3]
-        return np.array([V, f(R,U,V), W, df_du(R,U,V)*Z + df_dv(R,U,V)*W])
 
+# The actual loop that implements the shooting method
+while k <= maxIter and (abs(u_1-b)>tol):
     # Integrator. I could use RK45 but it seems to struggle with the higher vorticities, so this higher order RK method is better
     soln=solve_ivp(du_dr, r_span, ICs, t_eval=rs, method='DOP853')
 
@@ -71,17 +73,18 @@ while k <= maxIter and (abs(u_1-b)>tol):
     p=p-(u_1-beta)/(z_1)
     ICs[1]=p
     
-    print(k)
+    # print(k)
     k+=1
 
-print("p",p)
-print('z_1', z_1)
+print("Final p:",p)
+# print('z_1', z_1)
 
 # r values for plotting. They should all be the same across u, v, z, w. I called it t because that's what the integrator calls it
 t=soln.t
 
 # Plotting details
 plt.plot(t, u, label='u')
+plt.plot(t,0.1*t)
 # plt.plot(u_0_t, u_0, label='u0')
 
 plt.legend()
