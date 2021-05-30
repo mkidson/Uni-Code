cf0dd075b89a000a470d21e3e53603fe7547e8f7
MilesKidson <miles.kidson@gmail.com>
2021-05-30 12:55:23 +0200

    Starting AN proj 2

---
+++ 3rd Year/3AN/Project 2/NLS_splitStep.py
@@ -0,0 +1,12 @@
+from matplotlib import pyplot as plt
+import numpy as np
+from numpy import cos, pi, sin, sqrt, exp, random
+import matplotlib
+# matplotlib.use('pgf')
+matplotlib.rcParams.update({
+    'pgf.texsystem': 'pdflatex',
+    'font.family': 'serif',
+    'text.usetex': True,
+    'pgf.rcfonts': False,
+})
+
