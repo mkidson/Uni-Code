76025b9fb1fad5c0b04044be70fa9ee57a85de4f
MilesKidson <miles.kidson@gmail.com>
2021-09-17 17:37:36 +0200

    gamma gamma done

--- 3rd Year/PHY3004W/Labs/Gamma Gamma/Data/angularCorrelation.py
+++ 3rd Year/PHY3004W/Labs/Gamma Gamma/Data/angularCorrelation.py
@@ -4,10 +4,10 @@ import pandas as pd
 from scipy.optimize import curve_fit
 from numpy import cos, pi, sin, sqrt, exp, random
 import matplotlib
-# matplotlib.use('pgf')
+matplotlib.use('pgf')
 matplotlib.rcParams.update({
     'pgf.texsystem': 'pdflatex',
-    'font.family': 'serif',
+    'font.family': 'sans-serif',
     'text.usetex': True,
     'pgf.rcfonts': False,
     'figure.constrained_layout.use': True,
@@ -15,22 +15,31 @@ matplotlib.rcParams.update({
     'axes.labelsize': 16,
     'legend.fontsize': 16,
     'xtick.labelsize': 12,
-    'ytick.labelsize': 12
+    'ytick.labelsize': 12,
+    # 'text.latex.preamble': r'\usepackage{amsmath}'
 })
 
 names = ['PHY3004W_gglab_Angular_Scan1.csv', 'PHY3004W_gglab_Angular_Scan2.csv', 'PHY3004W_gglab_Angular_Scan3.csv']
 
 for name in names:
     data = np.genfromtxt(f'PHY3004W_gglab_angular_scans/{name}', delimiter=',', skip_header=8, unpack=True)
-
+    # data[1:3]/=30
 
     plt.figure()
     # print(data)
-    plt.scatter(data[0], data[1], label='Red', c='red', s=8)
-    plt.scatter(data[0], data[2], label='Blue', c='blue', s=8)
-    plt.plot(data[0], data[3], label='Coincident Events', color='g')
-    plt.legend()
-plt.xlabel('Angle (degrees)')
-plt.ylabel('Coincident gammas count')
+    plt.scatter(data[0], data[1], label=r'$N_{\mathrm{red}}$', c='red', s=8)
+    plt.scatter(data[0], data[2], label=r'$N_{\mathrm{blue}}$', c='blue', s=8)
+    plt.plot(data[0], data[3], label=r'$N_{\mathrm{coincident}}$', color='g')
+    plt.legend(loc='lower right')
+    plt.xlabel('Angle (degrees)')
+    plt.ylabel('Counts of gammas')
+    plt.grid(color='#CCCCCC', linestyle=':')
+    # print('--------------------------------')
+    # print(f'Run {name[-5]}')
+    # ratio = data[3]/data[1]
+    # ratioUn = ratio*sqrt((sqrt(data[3])/data[3])**2 + (sqrt(data[1])/data[1])**2)
+    # for i in range(len(data[0])):
+    #     print(f'{data[0][i]}: {ratio[i]} +/- {ratioUn[i]}')
+    plt.savefig(f'Plots/angular_{name[-5]}.pgf')
 # plt.xlim(-100, 95)
-plt.show()
+# plt.show()
--- 3rd Year/PHY3004W/Labs/Gamma Gamma/Data/coincidentGamma.py
+++ 3rd Year/PHY3004W/Labs/Gamma Gamma/Data/coincidentGamma.py
@@ -4,7 +4,7 @@ import pandas as pd
 from scipy.optimize import curve_fit
 from numpy import cos, pi, sin, sqrt, exp, random
 import matplotlib
-matplotlib.use('pgf')
+# matplotlib.use('pgf')
 matplotlib.rcParams.update({
     'pgf.texsystem': 'pdflatex',
     'font.family': 'sans-serif',
@@ -20,10 +20,13 @@ matplotlib.rcParams.update({
 })
 
 
-# names = ["PHY3004W_gglab_BlueCo60_gated1.csv", "PHY3004W_gglab_BlueCo60_gated2.csv", "PHY3004W_gglab_BlueNa22_gated1.csv", "PHY3004W_gglab_BlueNa22_gated2.csv", "PHY3004W_gglab_RedCo60_gated1.csv", "PHY3004W_gglab_RedNa22_gated1.csv"]
+# names = ["PHY3004W_gglab_BlueCo60_gated1.csv", "PHY3004W_gglab_BlueCo60_gated2.csv"] 
+# names = ["PHY3004W_gglab_BlueNa22_gated1.csv", "PHY3004W_gglab_BlueNa22_gated2.csv"]#, "PHY3004W_gglab_RedCo60_gated1.csv", "PHY3004W_gglab_RedNa22_gated1.csv"]
 
 name = "PHY3004W_gglab_RedCo60_gated1.csv"
 
+plt.figure()
+ax=plt.axes()
 
 # for name in names:
 # region Ingest Data
@@ -37,25 +40,45 @@ elif name[15:18]=='Red':
 # data[1] /= liveTime
 
 # endregion
-plt.figure()
-ax=plt.axes()
 ax.set_box_aspect(5/14) # for wide boys use 5/14
 
 ax.set_xlabel('Energy (keV)')
 ax.set_ylabel('Counts')
 
-ax.step(data[0], data[1], lw=1)
-ax.fill_between(data[0][0:330],data[1][0:330], step='pre', color='green', alpha=0.5, label='Compton continuum for 1172 keV photopeak')
-ax.axvline(data[0][58], linestyle='dashed', color='black', linewidth=1)
-ax.axvline(data[0][68], linestyle='dashed', color='black', linewidth=1)
-ax.text(data[0][57], 800, 'Possible 1172 keV \ndouble escape peak', rotation='vertical', horizontalalignment='right', size=12)
+# if names.index(name)==0:
+#     label='TSCA window wide open'
+# elif names.index(name)==1:
+#     label='TSCA window centred on \n511 keV photopeak'
+
+ax.step(data[0], data[1], lw=1)#, label=label)
 
 
+# ax.set_xlim(left=140,right=1400)
 ax.set_xlim(left=0,right=1700)
-# ax.set_xlim(left=0,right=600)
+# ax.set_xlim(left=0,right=278)
 ax.set_ylim(bottom=0)
 
 
 ax.legend()
-# plt.show()
-plt.savefig(r'Plots/60Co_Gated.pgf')
+plt.show()
+# plt.savefig(r'Plots/.pgf')
+
+# region red_60Co_gated
+# ax.fill_between(data[0][0:330],data[1][0:330], step='pre', color='green', alpha=0.5, label='Compton continuum for 1172 keV photopeak')
+# ax.axvline(data[0][58], linestyle='dashed', color='black', linewidth=1)
+# ax.axvline(data[0][68], linestyle='dashed', color='black', linewidth=1)
+# ax.text(data[0][72], 800, 'Possible 1172 keV \ndouble escape peak', rotation='horizontal', horizontalalignment='left', size=12)
+# ax.axvline(data[0][412], linestyle='dashed', color='black', linewidth=1)
+# ax.text(data[0][420], 1200, '1172 keV photopeak', rotation='horizontal', horizontalalignment='left', size=12)
+# ax.axvline(data[0][466], ymax=0.5, linestyle='dashed', color='black', linewidth=1)
+# ax.text(data[0][470], 500, 'Remnants of the \n1332 keV \nphotopeak', rotation='horizontal', horizontalalignment='left', size=12)
+# ax.set_xlim(left=0,right=1700)
+# endregion
+
+# region red_22Na_gated
+# ax.fill_between(data[0][0:115],data[1][0:115], step='pre', color='green', alpha=0.5, label='Compton continuum for \n511 keV photopeak')
+# ax.axvline(data[0][178], linestyle='dashed', color='black', linewidth=1)
+# ax.text(data[0][185], 25000, '511 keV photopeak', rotation='horizontal', horizontalalignment='left', size=12)
+# ax.set_xlim(left=0,right=800)
+# endregion
+
