2baadb7f6bd128840e139bb5f6faeec2c0ea4bbe
MilesKidson <miles.kidson@gmail.com>
2022-04-27 13:49:32 +0200

    tut2 CP q2 working!

--- Honours/CP/Tut 2/tut2.py
+++ Honours/CP/Tut 2/tut2.py
@@ -26,13 +26,13 @@ def lagrangePolynomial(x, i, xs, N=None):
     -------
     p : (float) Value of the Lagrange polynomial at point `x`
     """
-    if N == None:
+    if N == None:       # Setting a default value for N
         N = len(xs)
 
-    p = 1
+    p = 1       # Can't be 0, lol
 
     for j in range(N):
-        if j == i:
+        if j == i:      # Skips j=i term
             continue
         p *= (x - xs[j]) / (x[i] - x[j])
 
@@ -75,7 +75,7 @@ def q1():
 def gaussianKernel(x, xPrime, h):
     return (1/(h*np.sqrt(np.pi)))*np.exp(-((x-xPrime)/h)**2)
 
-def smoothParticleInterpolation(x, xs, ys, h, xEvalRange=None):
+def smoothParticleInterpolation(x, xs, ys, h):
     """
     Returns the value at `x` of the interpolated function, using Smooth Particle Interpolation, over the data pairs `(xs[i], ys[i])`. A range of evaluation can be specified, but defaults to `[min(xs), max(xs)]`.
 
@@ -89,26 +89,23 @@ def smoothParticleInterpolation(x, xs, ys, h, xEvalRange=None):
 
     h : (float) So-called "smoothing length" for the kernel function
 
-    xEvalRange : (tuple)(float) Tuple of the bound of the interval over which to evaluate the interpolation
-
     Returns
     -------
     y : (float) Value of the the interpolated function at `x`. Note that for `x = xs[i]`, this may not necessarily return `ys[i]
     """
-    if xEvalRange == None:
-        xEvalRange = (min(xs), max(xs))
-    
+    # Making them numpy arrays because they're just better to work with
     xs = np.array(xs)
     ys = np.array(ys)
 
     y = 0
 
     xSpacing = []
-    for i in range(len(xs[(xs >= xEvalRange[0]) and (xs <= xEvalRange[1])])):
+    for i in range(len(xs)):
+        # Finds the spacing between "particles" by finding the distance between the midpoint of x[i] and x[i+1] and the midpoint of x[i-1] and x[i]. For the first and last points it just takes the distance between it and the point above or below it.
         if i == 0:
             xSpacing.append((xs[i+1] - xs[i]))
 
-        elif i == len(xs):
+        elif i == len(xs)-1:
             xSpacing.append((xs[i] - xs[i-1]))
 
         else:
@@ -116,19 +113,35 @@ def smoothParticleInterpolation(x, xs, ys, h, xEvalRange=None):
 
     y = np.sum(xSpacing*ys*gaussianKernel(x, xs, h))
 
+    return y
+
 def q2eqn(x):
     return 3 * x**4 - 3 * x**2
 
-def q2():
+def q2(xMin, xMax, hs):
+    # Making the data from the given eqn
     xData = np.arange(-10, 10.1, 0.1)
     yData = q2eqn(xData)
+    
+    xEval = np.linspace(xMin, xMax, 1000)       # Making the array of points on which to evaluate the interpolation
+    yExact = q2eqn(xEval)       # Finding the exact value of the function for the points we're going to interpolate for
 
-    plt.plot(xData, yData)
-    plt.plot(xData, smoothParticleInterpolation(xData, xData, yData, 0.05))
+    for i in hs:      # Trying out a range of h values 
+        yInterp = []
+        for x in xEval:
+            yInterp.append(smoothParticleInterpolation(x, xData, yData, i))
 
+        plt.plot(xEval, np.abs(yInterp-yExact), 's', ms=2, label=f'h = {i}')
+
+
+    # plt.plot(xData, yData, 'o')
+    # plt.plot(xData, yInterp, 's')
+
+    plt.legend()
     plt.show()
+    
 
 if __name__ == "__main__":
     # q1()
 
-    q2()
+    q2(-8, 8, [0.05, 0.1, 0.2, 0.4, 1])
