8053e835e7abcc42cfaaec37e48a2a8ed4d6ddf7
MilesKidson <miles.kidson@gmail.com>
2022-05-08 16:09:05 +0200

    CP tut3 done

--- Honours/CP/Tut 3/tut3.py
+++ Honours/CP/Tut 3/tut3.py
@@ -1,36 +1,8 @@
 # KDSMIL001
 # 05-05-2022
 
-from matplotlib import pyplot as plt
 import numpy as np
 import random
-from scipy.integrate import nquad
-
-def xInverse(u):
-    return -3 * np.log(-u / 3 + np.exp(1 / 3))
-
-def X(ux, uy, uz):
-    return np.exp(-ux / 3)
-
-def q1eqn(x, y, z):
-    return np.exp(-x / 3) * (1 + 0.1 * np.log(np.sqrt(x**2 + y**2 + z**2 + 1)))
-
-def q1():
-    N = 1000000
-
-    ux = np.random.rand(N)
-    xs = xInverse(ux)
-    ys = np.random.uniform(0, 5, N)
-    zs = np.random.uniform(0, 5, N)
-
-    normConst = -3 * (np.exp(-2) - np.exp(1/3)) * 25
-
-    I = (normConst / N) * np.sum(q1eqn(xs, ys, zs) / X(xs, ys, zs))
-
-    print(I)
-
-def q2eqn(x):
-    return np.exp(-x)
 
 def metropolisMethod(N, probDist, xMin, xMax, x0, Delta=1, sepConstant=1, ignoreInitial=0):
     """Metropolis Method
@@ -100,30 +72,84 @@ def metropolisMethod(N, probDist, xMin, xMax, x0, Delta=1, sepConstant=1, ignore
             else:
                 continue
 
+    numAccepted = numAccepted/sepConstant + ignoreInitial
+
     return acceptedXs[sepConstant*ignoreInitial::sepConstant], numAccepted
 
+def autocorrelation(x, j):
+    return (np.mean(x[:-j]*x[j:])-np.mean(x[:-j])**2)/(np.mean(x[:-j]**2)-np.mean(x[:-j])**2)
+
+def xInverse(u):
+    return -3 * np.log(-u / 3 + np.exp(1 / 3))
+
+def X(ux, uy, uz):
+    return np.exp(-ux / 3)
+
+def q1eqn(x, y, z):
+    return np.exp(-x / 3) * (1 + 0.1 * np.log(np.sqrt(x**2 + y**2 + z**2 + 1)))
+
+def q2eqn(x):
+    return np.exp(-x)
+
+def q1():
+    N = 1000000     # Number of points to generate for x, y, and z
+
+    numRuns = 100   # Number of runs to do
+    ints = []       # Array for integral values
+
+    for i in range(numRuns):
+        ux = np.random.rand(N)
+        xs = xInverse(ux)       # Generates values distributed according to e^{-x/3}
+        ys = np.random.uniform(0, 5, N)     # Uniform on [0,5]
+        zs = np.random.uniform(0, 5, N)
+
+        normConst = -3 * (np.exp(-2) - np.exp(1/3)) * 25    # Defining the normalisation constant so that w(x,y,z) is normalised over the interval
+
+        I = (normConst / N) * np.sum(q1eqn(xs, ys, zs) / X(xs, ys, zs))     # Calculating I
+
+        ints.append(I)
+
+    intMean = np.mean(ints)
+    intStd = np.std(ints)
+    intUnc = intStd / np.sqrt(numRuns)
+
+    print(f'I = {intMean} +/- {intUnc}')
+
 def q2():
-    N = 10000000
-    ran = (0, 10000)
-    # plotRange = np.linspace(minPlot, maxPlot,1000)
-    xs, num = metropolisMethod(N, q2eqn, ran[0], ran[1], 0, 2)
+    N = 30000       # Number of points to generate
+    ran = (0, np.inf)   # range over which metropolis will run
 
-    I = np.sum(xs) / N
-    print(I)
+    numRuns = 100       # Number of runs to perform
+    ints = []       # Array for integral values
 
-    # print(np.max(q2eqn(np.linspace(0, 100, 1000))))
-    # print(num/N)
-    # plt.hist(xs, 20, (0, 10), density=True)
-    # plt.plot(plotRange, q2eqn(plotRange))
-    # plt.show()
+    for i in range(numRuns):
+
+        xs, num = metropolisMethod(N, q2eqn, ran[0], ran[1], 0, 2, 58, 100)
+
+        # autoCors = []
+        # for i in np.arange(1,1000):
+        #     autoCor = autocorrelation(np.array(xs), i)
+        #     autoCors.append(autoCor)
+        #     if np.abs(autoCor) <= 1e-3:
+        #         # print(f'C(j={i}) = 0')
+        #         seps.append(i)
+        #         break
+
+        I = np.sum(xs) / len(xs)    # Calculating the integral
+        ints.append(I)
+
+
+
+    intMean = np.mean(ints)
+    intStd = np.std(ints)
+
+    print(f'I = {intMean} +/- {intStd/np.sqrt(numRuns)}')
+
 
 if __name__ == "__main__":
     
-    q1()
-    
-    # q2()
-    
-    I = nquad(q1eqn, [[-1, 6], [0, 5], [0, 5]])
-    print(I)
-    
+    # q1()
+
+    q2()
+
     pass
