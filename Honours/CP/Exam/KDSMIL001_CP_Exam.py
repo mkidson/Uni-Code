cad5dbf48f8e85565046bc8c83fe802d8a24d6cc
MilesKidson <miles.kidson@gmail.com>
2022-06-12 17:38:12 +0200

    cp exam report half done

--- Honours/CP/Exam/KDSMIL001_CP_Exam.py
+++ Honours/CP/Exam/KDSMIL001_CP_Exam.py
@@ -51,7 +51,7 @@ def boltzmann(p):
     return p**2 * np.exp(- p / 2)
 
 def boltzmannNorm(p):
-    return p**2 * np.exp(- p / 2) / 15.999371064829049
+    return p**2 * np.exp(- p / 2) / 16
 
 def thetaDist(x):
     return np.arccos(1 - 2 * x)
@@ -119,56 +119,62 @@ def regulaFalsi(x1, x2, func, rootVal=0.0, tol=0.01, printIntermediate=True):
 def q1a():
     # Using rejection method to generate gaussian distribution
     photonYields = rejectionMethod(20000, gaussian, 9500, 10500)
+    print(f'First yield generated: {photonYields[0]}')
 
+    # Finding average and variance
     yieldAvg = np.mean(photonYields)
     print(f'Average yield: {yieldAvg}')
     yieldVar = np.var(photonYields)
     print(f'Variance of yields: {yieldVar}')
 
     xRange = np.linspace(9500, 10500, 1000)
-    plt.hist(photonYields, bins=200, density=True, alpha=0.8, fc='C0', ec='blue', label='Rejection Method')
+    plt.hist(photonYields, bins=100, density=True, alpha=0.8, fc='C0', ec='blue', label='Rejection Method')
     plt.plot(xRange, gaussian(xRange), color='red', label='Expected Heights')
     plt.legend()
-    plt.title('Distribution of photon yields')
+    plt.title('Distribution of photon yields,, normalised')
     plt.xlabel('Number of photons')
     plt.show()
 
 def q1b():
-    pRange = np.linspace(0, 30, 1000)
+    pRange = np.linspace(0, 30, 1000)       # Distribution drops off appreciably by p=30
 
     plt.figure()
     plt.plot(pRange, boltzmann(pRange))
     plt.xlabel('p (MeV)')
     plt.title('Unnormalised Boltzmann distribution for momentum')
+    plt.grid(color='#CCCCCC', linestyle=':')
 
-    norm = quad(boltzmann, 0, 30)
+    norm = quad(boltzmann, 0, np.inf)       # Normalisation, might as well go to infinity if we can
     print(norm)
 
     plt.figure()
     plt.plot(pRange, boltzmannNorm(pRange))
     plt.xlabel('p (MeV)')
     plt.title('Normalised Boltzmann distribution for momentum')
+    plt.grid(color='#CCCCCC', linestyle=':')
 
     plt.show()
 
 def q1cf():
-    numPhotons = 10000      # Change this on the final run
-    pRange = np.linspace(0, 30, 1000)
+    # This is the code for both 1c and 1f as they kind of do the same thing/rely on the same inputs, namely the number of photons to generate
 
-    photonMomenta = rejectionMethod(numPhotons, boltzmannNorm, 0, 30)
+    numPhotons =  9759      
+    pRange = np.linspace(0, 30, 1000)       # Range of momenta
+
+    photonMomenta = rejectionMethod(numPhotons, boltzmannNorm, 0, 30)       # Using rejection to get the photon momentum distribution
     momentaAvg = np.mean(photonMomenta)
 
     print(f'Average momenum magnitude: {momentaAvg}')
     
-    thetas = thetaDist(np.random.rand(numPhotons))
+    thetas = thetaDist(np.random.rand(numPhotons))      # Using inverse transform to find theta and phi distributions
     phis = phiDist(np.random.rand(numPhotons))
 
-    avgZMomentum = np.mean(photonMomenta * np.cos(thetas))
+    avgZMomentum = np.mean(photonMomenta * np.cos(thetas))      # Average momentum in z direction is p*cos(theta)
 
     print(f'Average momentum in z direction: {avgZMomentum}')
 
 
-    plt.hist(photonMomenta, bins=30, density=True, alpha=0.8, fc='C0', ec='blue', label='Rejection Method')
+    plt.hist(photonMomenta, bins=30, density=True, alpha=0.8, fc='C0', ec='blue', label='Random Numbers')
     plt.plot(pRange, boltzmannNorm(pRange), color='red', label='Expected Heights')
     plt.legend()
     plt.title('Distribution of photon momenta')
@@ -176,14 +182,14 @@ def q1cf():
 
 
     plt.figure()
-    plt.hist(thetas, 50, alpha=0.8, fc='C0', ec='blue')
-    plt.title(r'Distribution of $\theta$')
+    plt.hist(thetas, 50, density=True, alpha=0.8, fc='C0', ec='blue')
+    plt.title(r'Distribution of $\theta$, normalised')
     plt.xlabel(r'$\theta$')
     
     
     plt.figure()
-    plt.hist(phis, 50, alpha=0.8, fc='C0', ec='blue')
-    plt.title(r'Distribution of $\phi$')
+    plt.hist(phis, 50, density=True, alpha=0.8, fc='C0', ec='blue')
+    plt.title(r'Distribution of $\phi$, normalised')
     plt.xlabel(r'$\phi$')
 
 
@@ -211,26 +217,27 @@ def numerovDE0(E0):
     psi_0 = 0
     psi_1 = 0.0001       # A complete guess as to what is reasonable
 
-    psi_left = []
+    psi_left = []           # Initialisation of the left and right solutions
     psi_left.append(psi_0)
     psi_left.append(psi_1)
 
-    psi_right = []
+    psi_right = []          # Note that the right solution is being done in the right order, with its leftmost value in terms of x being the first in the array
     psi_right.append(psi_1)
     psi_right.append(psi_0)
 
-    # Will be going from -1 to 1 nm
+    # Chose -1 to 1 nm pretty arbitrarily, seemed to work well so I didn't change it
     xRange = np.linspace(-1, 1, 1000)
     deltaX = xRange[1] - xRange[0]
 
     potential = 0.5 * ( mc2 / hc**2 ) * hOmega**2 * xRange**2
 
-    meetPointX = np.where(potential <= E0)[0][-1]
+    meetPointX = np.where(potential <= E0)[0][-1]       # Finds the index of the classical turning point. Chose the right hand one
 
-    f = 2 * ( mc2 / hc**2 ) * ( E0 - potential )
+    f = 2 * ( mc2 / hc**2 ) * ( E0 - potential )        # From the derivation of Numerov
 
+    # Looping for the update equation. Took some thought to get the indexing right here but I'm happy with it
     for xl in xRange[1:meetPointX]:
-        i = np.where(xRange == xl)[0][0]
+        i = np.where(xRange == xl)[0][0]    # numpy arrays are really annoying sometimes but this works to get the index of an element. Could be an easier way to do this whole loop but it works
         psi_l_new = ( ( 2 - (5/6) * deltaX**2 * f[i] ) * psi_left[i] - ( 1 + (deltaX**2 / 12) * f[i-1] ) * psi_left[i-1] ) / ( 1 + (deltaX**2 / 12) * f[i+1])
         psi_left.append(psi_l_new)
     
@@ -240,13 +247,14 @@ def numerovDE0(E0):
         psi_r_new = ( ( 2 - (5/6) * deltaX**2 * f[c] ) * psi_right[0] - ( 1 + (deltaX**2 / 12) * f[c+1] ) * psi_right[1] ) / ( 1 + (deltaX**2 / 12) * f[c-1])
         psi_right.insert(0, psi_r_new)
 
-    psi_left = np.array(psi_left) * ( psi_right[1] / psi_left[-2] )
+    psi_left = np.array(psi_left) * ( psi_right[1] / psi_left[-2] )     # Adjusting left solution to match right at the turning point
 
-    dE0 = ( ( psi_left[-1] - psi_left[-3] ) - ( psi_right[2] - psi_right[0] ) ) / ( 2 * deltaX * psi_right[1])
+    dE0 = ( ( psi_left[-1] - psi_left[-3] ) - ( psi_right[2] - psi_right[0] ) ) / ( 2 * deltaX * psi_right[1])      # Finding d(E_0)
 
     return dE0
 
 def numerovWavefn(E0):
+    # This does exactly the same thing as the other numerov, but it just returns the wavefunction instead of d(E_0). The way that I originally set this all up meant that it was easier to just duplicate this instead of modifying the other one etc
     mc2 = 5.11e5            # eV
     hc = 197.3              # eV nm
     hOmega = 1              # eV
@@ -296,8 +304,9 @@ def numerovWavefn(E0):
 
 
 def q2a():
-    ERanges = [(0.1, 0.62), (1.3, 1.6), (2.35, 2.6)]
+    ERanges = [(0.1, 0.62), (1.3, 1.6), (2.35, 2.6)]        # Found ranges by plotting d(E_0) against E_0 and seeing where it crossed 0
 
+    # Simple regula falsi root finding
     E_0 = regulaFalsi(ERanges[0][0], ERanges[0][1], numerovDE0, tol=1e-4, printIntermediate=False)
     E_1 = regulaFalsi(ERanges[1][0], ERanges[1][1], numerovDE0, tol=1e-4, printIntermediate=False)
     E_2 = regulaFalsi(ERanges[2][0], ERanges[2][1], numerovDE0, tol=1e-4, printIntermediate=False)
@@ -306,16 +315,24 @@ def q2a():
 
 
 def q2b():
-    Es = [0.5007712065928163, 1.5010174165369703, 2.502924361232347]
+    Es = [0.5007712065928163, 1.5010174165369703, 2.502924361232347]        # Values of E found from q2a. Note these will be different to the output from that function as it rounds to look nicer, but these are the actual values
 
-    xRange = np.linspace(-1, 1, 1000)
+    xRange = np.linspace(-1, 1, 1000)       # Same range of x values from earlier, in nm
+    deltaX = xRange[1] - xRange[0]
+    # For plotting the potential, just looks nice :)
+    mc2 = 5.11e5            # eV
+    hc = 197.3              # eV nm
+    hOmega = 1              # eV
+    potential = 0.5 * ( mc2 / hc**2 ) * hOmega**2 * xRange**2
+    plt.plot(xRange, potential, color='black', lw='1', label='V(x)', ls='--')
 
-    for E in Es:
+
+    for E in Es:        # For each E value we find the wavefunction, normalise it, and plot
         wavefunc = numerovWavefn(E)
-        normalisation = np.sum(wavefunc**2)
+        normalisation = np.sum(wavefunc**2) * deltaX    # Seems like all that's needed for normalisation
 
-        wavefuncNormed = wavefunc / normalisation
-
+        wavefuncNormed = wavefunc / np.sqrt(normalisation)       # Normalising
+        wavefuncNormed += E     # Shifting up by their energy value, just think it makes it a bit more clear when plotting
         plt.plot(xRange[:-1], wavefuncNormed, label=f'$\psi_{Es.index(E)}$')
     
     plt.xlabel('x')
@@ -331,8 +348,8 @@ def q2b():
 
 # q1b()
 
-# q1cf()
+q1cf()
 
 # q2a()
 
-q2b()
+# q2b()
