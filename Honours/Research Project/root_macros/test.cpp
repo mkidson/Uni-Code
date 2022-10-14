dabb0aa61f08b2a05ff0ecc812ace0a9854e78d1
mkidson <miles.kidson@gmail.com>
2022-10-14 17:35:08 +0200

    plots!

--- Honours/Research Project/root_macros/test.cpp
+++
@@ -1,7 +0,0 @@
-void test() {
-    TFile* fr = new TFile("AnalysisResults_pass4_full_weird.root", "READ");
-
-    HistogramRegistry* histReg;
-    fr->GetObject("fwd-registry", HistogramRegistry);
-    // eta->Draw();
-}
